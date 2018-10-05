3f0a6648de02b5e753b8e0ff8d94be1039e9ca3a
abdullah zaqout <abdualla1@students.iugaza.edu.ps>
2018-10-05 12:10:18 +0300

    create carousel_items list in home.py and iterate throw it in carousel.html to create carousel in jinja

--- band/www/home.py
+++ band/www/home.py
@@ -2,4 +2,6 @@ from __future__ import unicode_literals
 import frappe
 
 def get_context(context):
-    return {}
+    carousel_items = [{'title': 'Chicago','image_src':'/assets/band/images/chicago.jpg','caption':'Thank you, Chicago - A night we won\'t forget.', 'active':'active'}, {'title': 'Los Angeles','image_src':'/assets/band/images/la.jpg','caption':'Even though the traffic was a mess, we had the best time playing at Venice Beach!'}, {'title': 'New York','image_src':'/assets/band/images/ny.jpg','caption':'The atmosphere in New York is lorem ipsum.' }]
+    
+    return {'carousel_items': carousel_items}
